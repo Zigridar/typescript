14d14a1b7f2c2df85af588dfff2eeeeb2f52d478
zigridar <safonenko.max@yandex.ru>
2021-01-21 22:11:38 +0500

    readonly

--- src/generic.ts
+++ src/generic.ts
@@ -6,3 +6,61 @@ const promise: Promise<string> = new Promise<string>((resolve) => {
 })
 
 promise.then(data => console.log(data))
+
+////////
+
+function mergeObjects<T extends object, R extends object>(first: T, second: R): T & R {
+    return Object.assign({}, first, second)
+}
+
+const max = mergeObjects({name: 'Max'}, {age: 22})
+
+console.log(max.name, max.age)
+
+///////
+
+interface ILength {
+    length: number
+}
+
+function withCount<T extends ILength>(value: T): {value: T, count: string} {
+    return {
+        value,
+        count: `value ${value.length}`
+    }
+}
+
+///////
+
+function getObjectValue<T extends object, R extends keyof T>(current: T, key: R): T[R] {
+    return current[key]
+}
+
+const person = {
+    name: 'Max',
+    age: 22
+}
+
+console.log(getObjectValue(person, 'name'))
+
+////
+
+class Collection<T extends string | number | boolean> {
+
+    constructor(private _items: T[] = []) {}
+
+    add(item: T) {
+        this._items.push(item)
+    }
+
+    get items() {
+        return this._items
+    }
+}
+
+const collection= new Collection([1, true, '', ])
+
+//////
+const arr: Readonly<Array<number>> = [1, 2, 3]
+
+const obj: Readonly<object> = { name: 'object' }
